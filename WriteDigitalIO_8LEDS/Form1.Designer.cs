75beba76bb4c6de523ba7d16ecbf5fed46f789e7
whiddenv <whiddenv@PHY-GWH244-04>
2025-02-23 16:33:26 -0500

    Add project files.

---
+++ WriteDigitalIO_8LEDS/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WriteDigitalIO_8LEDS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.btn_Quit = new System.Windows.Forms.Button();
+            this.physicalChannelComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(54, 334);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(140, 334);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(226, 334);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(312, 334);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(398, 334);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(80, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(484, 334);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(80, 17);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(570, 334);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(80, 17);
+            this.checkBox7.TabIndex = 6;
+            this.checkBox7.Text = "checkBox7";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(656, 334);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(80, 17);
+            this.checkBox8.TabIndex = 7;
+            this.checkBox8.Text = "checkBox8";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // btn_Quit
+            // 
+            this.btn_Quit.Location = new System.Drawing.Point(373, 383);
+            this.btn_Quit.Name = "btn_Quit";
+            this.btn_Quit.Size = new System.Drawing.Size(74, 38);
+            this.btn_Quit.TabIndex = 8;
+            this.btn_Quit.Text = "Quit";
+            this.btn_Quit.UseVisualStyleBackColor = true;
+            this.btn_Quit.Click += new System.EventHandler(this.btn_Quit_Click);
+            // 
+            // physicalChannelComboBox
+            // 
+            this.physicalChannelComboBox.FormattingEnabled = true;
+            this.physicalChannelComboBox.Location = new System.Drawing.Point(299, 265);
+            this.physicalChannelComboBox.Name = "physicalChannelComboBox";
+            this.physicalChannelComboBox.Size = new System.Drawing.Size(223, 21);
+            this.physicalChannelComboBox.TabIndex = 9;
+            this.physicalChannelComboBox.SelectedIndexChanged += new System.EventHandler(this.physicalChannelComboBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.physicalChannelComboBox);
+            this.Controls.Add(this.btn_Quit);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.Button btn_Quit;
+        private System.Windows.Forms.ComboBox physicalChannelComboBox;
+    }
+}
+
---
+++ WriteDigitalIO_8LEDS/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Drawing;
+using NationalInstruments.DAQmx;
+using System.Windows.Forms;
+
+namespace WriteDigitalIO_8LEDS
+{
+    public partial class Form1 : Form
+    {
+        NationalInstruments.DAQmx.Task digitalWriteTask = new NationalInstruments.DAQmx.Task(); //Creating a global NI task
+        DigitalSingleChannelWriter writer; // create global writer class... designed for writing to single-channel digital outputs
+        System.Windows.Forms.CheckBox[] chkArray; //global empty checkbox array
+
+
+        public Form1()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Has occured in initialization", ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chkArray = new System.Windows.Forms.CheckBox[8] { checkBox1, checkBox2, checkBox3, checkBox4, checkBox5, checkBox6, checkBox7, checkBox8 };
+            for (int i = 0; i < chkArray.Length; i++)
+            {
+                chkArray[i].Checked = false;
+            }
+                try
+            {
+                
+
+                physicalChannelComboBox.DropDownStyle = ComboBoxStyle.DropDownList; //changes the combobox to be Non-Editable
+                physicalChannelComboBox.Items.AddRange(DaqSystem.Local.GetPhysicalChannels(PhysicalChannelTypes.DOPort, PhysicalChannelAccess.External));
+                if (physicalChannelComboBox.Items.Count > 0)
+                {
+                    physicalChannelComboBox.SelectedIndex = 0;
+
+                }
+                else {
+                    MessageBox.Show("DAQ not on"); 
+                    this.Close();
+                }
+
+                //Using an EVENT HANDLER instead of the forms method "checkBox1_CheckedChanged"
+                //Continuously loops through each checkbox and sees if there is a change
+                foreach (System.Windows.Forms.CheckBox chk in chkArray)
+                {
+                    chk.CheckedChanged += new EventHandler(Uppdate); //Call Uppdate if there is a check changed
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occured in the form load", ex.Message);
+            }
+        }
+
+        private void Uppdate(object sender, EventArgs e)
+        {
+            try
+            {
+                int binaryValue = 0;
+
+                //For a given integer, until the end of the chkArray, increase i+1
+                for (int i = 0; i < chkArray.Length; i++)
+                {
+                    if (chkArray[i].Checked)
+                    {
+                        binaryValue |= (1 << i); //Shift a 1 into the location of i if there is a checkCHanged
+                        chkArray[i].BackColor = Color.Green; //Changes the color of checkbox to Green if 1
+                    }
+                    else
+                    {
+                        chkArray[i].BackColor = Color.Salmon; //changes color of CheckBx to SALMON if 0
+                    }
+                }
+
+                //boolean values (bool[]), where each element represents a digital output (either ON or OFF).
+                //Do exact same thing, but make a boolean array with logic
+
+                bool[] booleanArray = new bool[8];
+                for (int i = 0; i < 8; i++)
+                {
+                    booleanArray[i] = (binaryValue & (1 << i)) != 0; //Using != 0 allows handling of all possible non-zero results,
+                }
+
+                writer.WriteSingleSampleMultiLine(true, booleanArray); //Should send out the new array to every LED based off of if it is TRUE 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an error in the binary fetching", ex.Message);
+            }
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            bool[] booleanArray0 = { false, false, false, false, false, false, false, false };  // All values are false
+
+            try
+            {
+                if (digitalWriteTask != null && physicalChannelComboBox.Items.Count > 0) //Only dispose of the task if it exists
+                {
+                    writer.WriteSingleSampleMultiLine(true, booleanArray0); //Should send out the new array to every LED based off of if it is TRUE 
+
+                    digitalWriteTask.Dispose();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occured in the form close", ex.Message);
+            }
+        }
+
+        private void btn_Quit_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+        }
+
+        private void physicalChannelComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < chkArray.Length; i++)
+            {
+                chkArray[i].Checked = false;
+            }
+
+            try
+            {
+                if (digitalWriteTask != null) //Disposing a task if the combobox is changed
+                {
+                    digitalWriteTask.Dispose();
+                    digitalWriteTask = new NationalInstruments.DAQmx.Task();
+                    string selectedPort = physicalChannelComboBox.SelectedItem.ToString();
+                    digitalWriteTask.DOChannels.CreateChannel(selectedPort, "", ChannelLineGrouping.OneChannelForAllLines);
+                    writer = new DigitalSingleChannelWriter(digitalWriteTask.Stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occured when disposing a task for a combobox change", ex.Message);
+            }
+        }
+    }
+}
